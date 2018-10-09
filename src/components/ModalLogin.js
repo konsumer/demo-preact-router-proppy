b2c1f3f5390bc890084afdc54059134eda658889
David Konsumer <konsumer@jetboystudio.com>
2018-10-09 13:08:50 -0700

    required validation

--- src/components/ModalLogin.js
+++ src/components/ModalLogin.js
@@ -5,10 +5,10 @@ const ModalLogin = ({ onSubmit, onClickCancel }) => (
   <form onSubmit={onSubmit}>
     <h2>Login</h2>
     <div className='field'>
-      <input type='email' name='email' placeholder='email' autoFocus />
+      <input type='email' name='email' placeholder='email' autoFocus required />
     </div>
     <div className='field'>
-      <input type='password' name='password' placeholder='password' />
+      <input type='password' name='password' placeholder='password' required />
     </div>
     <div className='field'>
       <button onClick={onClickCancel}>CANCEL</button>
